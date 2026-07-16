bb3b411306b78dbb20d0432f3920de8f15210124
Jaabir N N <74167364+jaabir25@users.noreply.github.com>
2026-07-16 22:17:14 +0900

    Add files via upload

---
+++ denso_vp6242_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/maeda/Desktop/Mechanical_Analysis/ROS2_ws/src/denso_vp6242_moveit_config
+# Build directory: /home/maeda/Desktop/Mechanical_Analysis/ROS2_ws/build/denso_vp6242_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ denso_vp6242_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/maeda/Desktop/Mechanical_Analysis/ROS2_ws/install/denso_vp6242_description/share/denso_vp6242_description/package.sh"
