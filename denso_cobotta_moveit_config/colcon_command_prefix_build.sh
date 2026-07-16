5449412e61faef3779da18f7c112ecc4d963dfc5
Jaabir N N <74167364+jaabir25@users.noreply.github.com>
2026-07-16 22:14:57 +0900

    Add files via upload

---
+++ denso_cobotta_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/maeda/Desktop/Mechanical_Analysis/ROS2_ws/install/denso_cobotta_description/share/denso_cobotta_description/package.sh"
