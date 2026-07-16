9207188ea49095829d4cba72eb6e3cb21a44542d
Jaabir N N <74167364+jaabir25@users.noreply.github.com>
2026-07-16 22:35:31 +0900

    Add files via upload

---
+++ environment_interface__rosidl_typesupport_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/action/task_creator__type_support.cpp.o"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/action/task_creator__type_support.cpp.o.d"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/msg/block__type_support.cpp.o"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/msg/block__type_support.cpp.o.d"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/srv/block_create__type_support.cpp.o"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/srv/block_create__type_support.cpp.o.d"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/srv/block_remove__type_support.cpp.o"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/srv/block_remove__type_support.cpp.o.d"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/srv/block_remove_all__type_support.cpp.o"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/srv/block_remove_all__type_support.cpp.o.d"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/srv/get_block_color__type_support.cpp.o"
+  "CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/rosidl_typesupport_cpp/environment_interface/srv/get_block_color__type_support.cpp.o.d"
+  "libenvironment_interface__rosidl_typesupport_cpp.pdb"
+  "libenvironment_interface__rosidl_typesupport_cpp.so"
+  "rosidl_typesupport_cpp/environment_interface/action/task_creator__type_support.cpp"
+  "rosidl_typesupport_cpp/environment_interface/msg/block__type_support.cpp"
+  "rosidl_typesupport_cpp/environment_interface/srv/block_create__type_support.cpp"
+  "rosidl_typesupport_cpp/environment_interface/srv/block_remove__type_support.cpp"
+  "rosidl_typesupport_cpp/environment_interface/srv/block_remove_all__type_support.cpp"
+  "rosidl_typesupport_cpp/environment_interface/srv/get_block_color__type_support.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/environment_interface__rosidl_typesupport_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
