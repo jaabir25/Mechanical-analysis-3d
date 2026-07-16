492eef1cfbd08ead548f3520131fb83efbb49f38
Jaabir N N <74167364+jaabir25@users.noreply.github.com>
2026-07-16 22:28:59 +0900

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/maeda/Desktop/Mechanical_Analysis/ROS2_ws/src/environment_builder
+# Build directory: /home/maeda/Desktop/Mechanical_Analysis/ROS2_ws/build/environment_builder
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/maeda/Desktop/Mechanical_Analysis/ROS2_ws/install/environment_interface/share/environment_interface/package.sh"
